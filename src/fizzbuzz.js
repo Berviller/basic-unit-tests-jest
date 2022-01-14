aaa4b06e2737dfb6616da143e89ed8c210d081f4
Thomas <tberviller@gmail.com>
2022-01-14 17:35:59 +0100

    Réalisation des testes pour les scénarios fizz et buzz de la fonction fizzBuzz

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,12 +1,12 @@
 function fizzBuzz (n) {
-    if(n%3==0):
+    if(n%3==0)
 		return "Fizz"
-	if(n%5==0):
+	if(n%5==0)
 		return "Buzz"
-	if(n%3==0&&n%5==0):
+	if(n%3==0&&n%5==0)
 		return "FizzBuzz"
-	else:
-	return ""
+	else
+		return ""
 }
 
 module.exports = fizzBuzz
--- test/fizzbuzz.test.js
+++ test/fizzbuzz.test.js
@@ -1,5 +1,17 @@
 const fizzBuzz = require("../src/fizzbuzz")
 
-test('TODO', () => {
-    // TODO: Tester fizzbuzz()
+test('Scénario fizz', () => {
+	const expectResult = "Fizz"
+	
+    const result = fizzBuzz(3)
+	
+	expect(result).toBe(expectResult)
+})
+
+test("Scénario buzz", () =>{
+	const expectResult = "Buzz"
+
+	const result = fizzBuzz(5)
+
+	expect(result).toBe(expectResult)
 })
