a95576d89d7c2c7eeac43ed2b023dd793e795631
Thomas <tberviller@gmail.com>
2022-01-14 16:52:05 +0100

    Réalisation de FizzBuzz

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,6 +1,12 @@
 function fizzBuzz (n) {
-    // TODO: Compléter fizzbuzz()
-    return "fizzbuzz"
+    if(n%3==0):
+		return "Fizz"
+	if(n%5==0):
+		return "Buzz"
+	if(n%3==0&&n%5==0):
+		return "FizzBuzz"
+	else:
+	return ""
 }
 
 module.exports = fizzBuzz
