ef9351b607304061624449b8c56568b2606f519f
Thomas <tberviller@gmail.com>
2022-01-14 17:57:34 +0100

    Réordonnancement des vérification dans la fonction pour qu'elle fonctionne comme souhaitée

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,10 +1,10 @@
 function fizzBuzz (n) {
+	if(n%3==0&&n%5==0)
+		return "FizzBuzz"
     if(n%3==0)
 		return "Fizz"
 	if(n%5==0)
 		return "Buzz"
-	if(n%3==0&&n%5==0)
-		return "FizzBuzz"
 	else
 		return ""
 }
