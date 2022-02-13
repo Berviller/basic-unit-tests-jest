9833fc4b26aadd91028156768dc01e115fb41ca6
Thomas <tberviller@gmail.com>
2022-02-13 14:25:50 +0100

    Correction du programme pour la fusion de 2 tableaux

--- src/mergeSortedArrays.js
+++ src/mergeSortedArrays.js
@@ -1,14 +1,14 @@
 function mergeSortedArrays(t1, t2) {
-    size1 = t1.length()
-    size2 = t2.length()
+    size1 = t1.length
+    size2 = t2.length
     resultat = []
 
     for(i=0; i<size1; i++){
         if(t1[i]>t2[i]){
-            resultat.append(t2[i])
+            resultat.push(t2[i])
         }
         else{
-            resultat.append(t1[i])
+            resultat.push(t1[i])
         }
     }
     return resultat
